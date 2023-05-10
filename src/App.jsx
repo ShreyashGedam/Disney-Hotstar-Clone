de7274d5a7d87c15229f34b07685acf00a7831f0
Shreyash Gedam <shreyashakgedam@gmail.com>
2023-05-10 11:20:04 +0530

    formateed with prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
-
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import { Login } from './components/Login'
-import { Header } from './components/Header'
-
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Login } from "./components/Login";
+import { Header } from "./components/Header";
 
 function App() {
-
   return (
     <>
       <Header />
       <Routes>
-        <Route path='/' element={<Login />}></Route>
+        <Route path="/" element={<Login />}></Route>
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,3 @@
 export const Header = () => {
-    return (
-        <h1>HEADER</h1>
-    )
-}
+  return <h1>HEADER</h1>;
+};
